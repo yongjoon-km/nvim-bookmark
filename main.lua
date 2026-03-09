5fef6369cd2f0567104e590a078bb25d73cb7a19
yongjoon <penubokim@gmail.com>
2026-03-10 08:55:31 +0900

    Define bookmark structure and serialization

--- main.lua
+++ main.lua
@@ -7,5 +7,45 @@ end
 
 local file = io.open(storage, "w+")
 if file then
-    file.close()
+    file:close()
 end
+
+function save_bookmark(data)
+    local file = io.open(storage, "a+")
+    if not file then
+        return
+    end
+    file:write(data)
+    file:close()
+end
+
+function serialize_bookmark(bookmark)
+    return bookmark.path .. "%%" .. bookmark.line .. "\n"
+end
+
+function serialize_bookmark_list(bookmark_list)
+    local result = ""
+    for _, bookmark in pairs(bookmark_list) do
+        result = result .. serialize_bookmark(bookmark)
+    end
+    return result
+end
+
+local bookmark_list = {
+    {
+        path = "/home/yongjoon/repos/nvim-bookmark",
+        line = 22
+    },
+    {
+        path = "/home/yongjoon/repos/nvim-bookmark",
+        line = 23
+    },
+    {
+        path = "/home/yongjoon/repos/nvim-bookmark",
+        line = 24
+    },
+}
+
+local data = serialize_bookmark_list(bookmark_list)
+save_bookmark(data)
+
